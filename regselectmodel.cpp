e0c1a7a8e2518307e4b7530e8ef1e61e523677e2
artem.lab <artem.lab@gmail.com>
2024-10-18 10:58:07 +0500

    reg select model <- sort filter model

--- regselectmodel.cpp
+++ regselectmodel.cpp
@@ -5,149 +5,12 @@
 
 
 RegSelectModel::RegSelectModel(QObject* parent)
-    :QAbstractProxyModel(parent)
+    :QSortFilterProxyModel(parent)
 {
-
 }
 
 RegSelectModel::~RegSelectModel()
 {
-
-}
-
-
-QModelIndex RegSelectModel::index(int row, int column, const QModelIndex& parent) const
-{
-    //qDebug() << "RegSelectModel::index(" << row << ", " << column << parent << ")";
-
-    QAbstractItemModel* model = sourceModel();
-    if(model == nullptr) return QModelIndex();
-
-    QModelIndex source_index = model->index(row, column, parent);
-
-    return createIndex(row, column, source_index.internalPointer());
-}
-
-QModelIndex RegSelectModel::parent(const QModelIndex& child) const
-{
-    //qDebug() << "RegSelectModel::parent(" << child << ")";
-
-    if(!child.isValid()) return QModelIndex();
-
-    QAbstractItemModel* model = sourceModel();
-    if(model == nullptr) return QModelIndex();
-
-    QModelIndex source_parent = model->parent(child);
-
-    return createIndex(source_parent.row(), source_parent.column(), source_parent.internalPointer());
-}
-
-int RegSelectModel::rowCount(const QModelIndex& parent) const
-{    
-    //qDebug() << "RegSelectModel::rowCount(" << parent << ")";
-
-    QAbstractItemModel* model = sourceModel();
-    if(model == nullptr) return 0;
-
-    return model->rowCount(parent);
-}
-
-int RegSelectModel::columnCount(const QModelIndex& parent) const
-{
-    //qDebug() << "RegSelectModel::columnCount(" << parent << ")";
-
-    Q_UNUSED(parent);
-
-    /*QAbstractItemModel* model = sourceModel();
-    if(model == nullptr) return 0;*/
-
-    return ColumnsCount;
-}
-
-QModelIndex RegSelectModel::mapToSource(const QModelIndex& proxyIndex) const
-{
-    //qDebug() << "RegSelectModel::mapToSource(" << proxyIndex << ")";
-
-    if(!proxyIndex.isValid()) return QModelIndex();
-
-    QAbstractItemModel* model = sourceModel();
-    if(model == nullptr) return QModelIndex();
-
-    QModelIndex parent = proxyIndex.parent();
-
-    switch(proxyIndex.column()){
-    default:
-        break;
-    case COL_INDEX:
-        return model->index(proxyIndex.row(), RegListModel::COL_INDEX, parent);
-    case COL_NAME:
-        return model->index(proxyIndex.row(), RegListModel::COL_NAME, parent);
-    case COL_DESCR:
-        return model->index(proxyIndex.row(), RegListModel::COL_DESCR, parent);
-    }
-
-    return QModelIndex();
-}
-
-QModelIndex RegSelectModel::mapFromSource(const QModelIndex& sourceIndex) const
-{
-    //qDebug() << "RegSelectModel::mapFromSource(" << sourceIndex << ")";
-
-    if(!sourceIndex.isValid()) return QModelIndex();
-
-    QModelIndex parent = sourceIndex.parent();
-
-    switch(sourceIndex.column()){
-    default:
-        break;
-    case RegListModel::COL_INDEX:
-        return index(sourceIndex.row(), COL_INDEX, parent);
-    case RegListModel::COL_NAME:
-        return index(sourceIndex.row(), COL_NAME, parent);
-    case RegListModel::COL_DESCR:
-        return index(sourceIndex.row(), COL_DESCR, parent);
-    }
-
-    return QModelIndex();
-}
-
-QVariant RegSelectModel::headerData(int section, Qt::Orientation orientation, int role) const
-{
-    //qDebug() << "RegSelectModel::headerData(" << section << ", " << orientation << static_cast<Qt::ItemDataRole>(role) << ")";
-
-    QAbstractItemModel* model = sourceModel();
-    if(model == nullptr) return QVariant();
-
-    if(orientation == Qt::Horizontal){
-        switch(section){
-        default:
-            break;
-        case COL_INDEX:
-            section = RegListModel::COL_INDEX;
-            break;
-        case COL_NAME:
-            section = RegListModel::COL_NAME;
-            break;
-        case COL_DESCR:
-            section = RegListModel::COL_DESCR;
-            break;
-        }
-    }
-
-    return model->headerData(section, orientation, role);
-}
-
-
-QVariant RegSelectModel::data(const QModelIndex& index, int role) const
-{
-    //qDebug() << "RegSelectModel::data(" << index << ", " << static_cast<Qt::ItemDataRole>(role) << ")";
-
-    QAbstractItemModel* model = sourceModel();
-    if(model == nullptr) return QVariant();
-
-    QModelIndex source_index = mapToSource(index);
-
-    return model->data(source_index, role);
 }
 
 
@@ -172,4 +35,21 @@ Qt::ItemFlags RegSelectModel::flags(const QModelIndex& index) const
     if(ro->type() != ObjectType::VAR) flags &= ~Qt::ItemIsSelectable;
 
     return flags;
+}
+
+
+bool RegSelectModel::filterAcceptsColumn(int source_column, const QModelIndex& source_parent) const
+{
+    Q_UNUSED(source_parent);
+
+    switch(source_column){
+    default:
+        break;
+    case RegListModel::COL_INDEX:
+    case RegListModel::COL_NAME:
+    case RegListModel::COL_DESCR:
+        return  true;
+    }
+
+    return false;
 }
--- regselectmodel.h
+++ regselectmodel.h
@@ -1,39 +1,22 @@
 #ifndef REGSELECTMODEL_H
 #define REGSELECTMODEL_H
 
-#include <QAbstractProxyModel>
+#include <QSortFilterProxyModel>
 
 class RegListModel;
 
-class RegSelectModel : public QAbstractProxyModel
+class RegSelectModel : public QSortFilterProxyModel
 {
     Q_OBJECT
 public:
 
-    enum Columns {
-        COL_INDEX = 0,
-        COL_NAME = 1,
-        COL_DESCR = 2,
-        ColumnsCount = 3
-    };
-
     explicit RegSelectModel(QObject* parent = nullptr);
     ~RegSelectModel();
 
-    QModelIndex index(int row, int column, const QModelIndex& parent) const override;
-    QModelIndex parent(const QModelIndex& child) const override;
-    int rowCount(const QModelIndex& parent) const override;
-    int columnCount(const QModelIndex& parent) const override;
-
-    QModelIndex mapToSource(const QModelIndex& proxyIndex) const override;
-    QModelIndex mapFromSource(const QModelIndex& sourceIndex) const override;
-
-    QVariant headerData(int section, Qt::Orientation orientation, int role) const override;
-    QVariant data(const QModelIndex& index, int role) const override;
-
     Qt::ItemFlags flags(const QModelIndex& index) const override;
 
-private:
+protected:
+    bool filterAcceptsColumn(int source_column, const QModelIndex& source_parent) const override;
 };
 
 #endif // REGSELECTMODEL_H
